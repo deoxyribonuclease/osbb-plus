6c394158b6d01676fdf2bd152fa31ae341155d9f
Vlad <bondar.vladislav1121@vu.cdu.edu.ua>
2025-06-09 02:33:30 +0300

    Server fix

--- osbb-backend/server.js
+++ osbb-backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const sequelize = require('./db');
 
-const { ip, port } = require('./config');
 
 const app = express();
 app.use(cors());
@@ -26,54 +25,28 @@ require('./models/Osbb');
 require('./models/associations');
 
 sequelize.sync({ force: false })
-    .then(() => {
-        console.log('Database & tables created!');
-    })
-    .catch((error) => {
-        console.error('Error creating database tables:', error);
-    });
+    .then(() => console.log('Database & tables created!'))
+    .catch((error) => console.error('Error creating database tables:', error));
 
-const userRoute = require('./routes/userRoutes');
-app.use('/users', userRoute);
-
-const osbbRoute = require('./routes/osbbRoutes');
-app.use('/osbb', osbbRoute);
-
-const houseRoute = require('./routes/houseRoutes');
-app.use('/house', houseRoute);
-
-const apartmentRoute = require('./routes/apartmentRoutes');
-app.use('/apartment', apartmentRoute);
-
-const residentRoute = require('./routes/residentRoutes');
-app.use('/resident', residentRoute);
-
-const meterRoute = require('./routes/meterRoutes');
-app.use('/meter', meterRoute);
-
-const repairApplicationRoute = require('./routes/repairApplicationRoutes');
-app.use('/rep-apl', repairApplicationRoute);
-
-const notificationRoute = require('./routes/notificationRoutes');
-app.use('/notification', notificationRoute);
-
-const newsRoute = require('./routes/newsRoutes');
-app.use('/news', newsRoute);
-
-const paymentRoute = require('./routes/paymentRoutes');
-app.use('/payment', paymentRoute);
-
-const authRoute = require('./routes/authRoutes');
-app.use('/auth', authRoute);
-
-const expenseRoute = require('./routes/expenseRoutes');
-app.use('/expense', expenseRoute);
+app.use('/users', require('./routes/userRoutes'));
+app.use('/osbb', require('./routes/osbbRoutes'));
+app.use('/house', require('./routes/houseRoutes'));
+app.use('/apartment', require('./routes/apartmentRoutes'));
+app.use('/resident', require('./routes/residentRoutes'));
+app.use('/meter', require('./routes/meterRoutes'));
+app.use('/rep-apl', require('./routes/repairApplicationRoutes'));
+app.use('/notification', require('./routes/notificationRoutes'));
+app.use('/news', require('./routes/newsRoutes'));
+app.use('/payment', require('./routes/paymentRoutes'));
+app.use('/auth', require('./routes/authRoutes'));
+app.use('/expense', require('./routes/expenseRoutes'));
 
 app.get('/', (req, res) => {
     res.send('<h1>Server is running!</h1>');
 });
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, ip, () => {
-    console.log(`Server is running on: ${ip}:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
